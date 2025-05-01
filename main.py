b3db93afcdbd7fbd602dd98f6d76144c8cd3f0e2
cr-beck <beck_cr@proton.me>
2025-04-30 19:17:18 -0500

    finish printing outputs

--- main.py
+++ main.py
@@ -9,14 +9,24 @@ def get_book_text(book):
     
 
 def main():
-    #num_words = get_book_word_count("books/frankenstein.txt")
+    num_words = get_book_word_count("books/frankenstein.txt")
     #print(f"{num_words} words found in the document")
     #print(get_book_character_count("books/frankenstein.txt"))
     character_count = get_book_character_count("books/frankenstein.txt")
     sorted_list = sort_character_count(character_count)
-    
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
     for dict in sorted_list:
-        for char in dict:
+        #print(dict["char"])
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["num"]}")
+
+    print("============= END ===============")
 
 
 main()
